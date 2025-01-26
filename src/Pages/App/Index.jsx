0c3b63e44343dbd6aed2a461547057d4d75a95aa
Juan <jcalderongomez@gmail.com>
2025-01-26 16:25:43 -0500

    clase 4

--- src/Pages/App/Index.jsx
+++ src/Pages/App/Index.jsx
@@ -1,24 +1,42 @@
-
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home/Index'
 import MyAccount from '../MyAccount/Index'
 import MyOrder from '../MyOrder/Index'
 import MyOrders from '../MyOrders/Index'
 import NotFound from '../NotFound/Index'
 import SignIn from '../SignIn/Index'
+import Navbar from '../../Components/Navbar/Index'
 import './App.css'
 
-function Index() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/clothes', element: <Home /> },
+    { path: '/electronics', element: <Home /> },
+    { path: '/furnitures', element: <Home /> },
+    { path: '/toys', element: <Home /> },
+    { path: '/othes', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/my-orders/last', element: <MyOrder /> },
+    { path: '/my-orders/:id', element: <MyOrder /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> },
+  ])
 
-  return (
-    <div className='bg-red-100'>
-      <Home />
-      <MyOrder />
-      <MyOrders />
-      <MyAccount />
-      <NotFound />
-      <SignIn />
-    </div>
-  )
+  return routes
 }
 
-export default Index
+
+const App = () => {
+  return (
+
+    <BrowserRouter>
+      <AppRoutes />
+      <Navbar />
+    </BrowserRouter>
+
+  )
+}
+export default App
