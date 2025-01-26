5209a1de4b7c3a24fe8dfd1011079668b33e0780
Juan <jcalderongomez@gmail.com>
2025-01-26 18:27:28 -0500

    clase 11

---
+++ src/Components/ProductDetail/index.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+const ProductDetail = () => {
+    return (
+        <aside className="product-detail flex flex-col fixed right-0 border border-black bg-white rounded-lg">
+            <div className='flex justify-between items-center'>
+                <h2 className='font-medium text-xl p-6'>Detail</h2>
+                <div>x</div>
+            </div>
+        </aside>
+    )
+}
+
+export default ProductDetail
