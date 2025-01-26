674da0b561d819c7c2f79576bb6f94f9ffc180e4
Juan <jcalderongomez@gmail.com>
2025-01-26 18:00:33 -0500

    clase 9

--- src/Components/Navbar/Index.jsx
+++ src/Components/Navbar/Index.jsx
@@ -1,7 +1,10 @@
 import { NavLink } from "react-router-dom"
-
+import { ShoppingCartContext } from "../../Context"
+import { useContext } from "react"
 const Navbar = () => {
   const activeStyle = 'underline underline-offset-4'
+  
+      const context = useContext(ShoppingCartContext)
   return (
     <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light">
       <ul className="flex items-center gap-3">
@@ -99,7 +102,7 @@ const Navbar = () => {
         </li>
         <li>
 
-          🛒 0
+          🛒 {context.count}
 
         </li>
       </ul>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,10 +1,15 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-const ShoppingCartContext = createContext()
+export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children})=>{
+    const [count,setCount] = useState(0)
+   console.log('count:' ,count )
     return (
-        <ShoppingCartContext.Provider>
+        <ShoppingCartContext.Provider value={{
+            count,
+            setCount
+        }}>
             {children}
         </ShoppingCartContext.Provider>
     )
