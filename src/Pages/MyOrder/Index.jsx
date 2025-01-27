31071e88a02d59206cb45de6ad76376672536738
Juan <jcalderongomez@gmail.com>
2025-01-27 00:34:12 -0500

    clase 21

--- src/Pages/MyOrder/Index.jsx
+++ src/Pages/MyOrder/Index.jsx
@@ -1,11 +1,27 @@
 import Layout from "../../Components/Layout"
-
+import OrderCard from "../../Components/OrderCard"
+import { ShoppingCartContext } from "../../Context"
+import { useContext } from 'react'
 
 function MyOrder() {
 
+  const context = useContext(ShoppingCartContext)
   return (
     <Layout>
       MyOrder
+      <div className='flex flex-col w-80'>
+        {
+          context.order?.slice(-1)[0].products.map(product => (
+            <OrderCard
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              imageUrl={product.images}
+              price={product.price}
+            />
+          ))
+        }
+      </div>
     </Layout>
   )
 }
