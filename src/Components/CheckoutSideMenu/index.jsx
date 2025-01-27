62c4ad5d2f08dd41ee32d99dd6716800afd28fb3
Juan <jcalderongomez@gmail.com>
2025-01-26 23:35:25 -0500

    clase 18

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -5,33 +5,40 @@ import OrderCard from '../../Components/OrderCard'
 import './styles.css'
 
 const CheckoutSideMenu = () => {
-  const context = useContext(ShoppingCartContext)
+    const context = useContext(ShoppingCartContext)
 
-  return (
-    <aside
-      className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
-      <div className='flex justify-between items-center p-6'>
-        <h2 className='font-medium text-xl'>My Order</h2>
-        <div>
-          <XMarkIcon
-            className='h-6 w-6 text-black cursor-pointer'
-            onClick={() => context.closeCheckoutSideMenu()}></XMarkIcon>
-        </div>
-      </div>
-      <div className='px-6 overflow-y-scroll'>
-        {
-          context.cartProducts.map(product => (
-            <OrderCard
-              key={product.id}
-              title={product.title}
-              imageUrl={product.images}
-              price={product.price}
-            />
-          ))
-        }
-      </div>
-    </aside>
-  )
+    const handleDelete = (id) => {
+        const filteredProducts = context.cartProducts.filter(product => product.id != id)
+        context.setCartProducts(filteredProducts)
+    }
+
+    return (
+        <aside
+            className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
+            <div className='flex justify-between items-center p-6'>
+                <h2 className='font-medium text-xl'>My Order</h2>
+                <div>
+                    <XMarkIcon
+                        className='h-6 w-6 text-black cursor-pointer'
+                        onClick={() => context.closeCheckoutSideMenu()}></XMarkIcon>
+                </div>
+            </div>
+            <div className='px-6 overflow-y-scroll'>
+                {
+                    context.cartProducts.map(product => (
+                        <OrderCard
+                            key={product.id}
+                            id={product.id}
+                            title={product.title}
+                            imageUrl={product.images}
+                            price={product.price}
+                            handleDelete={handleDelete}
+                        />
+                    ))
+                }
+            </div>
+        </aside>
+    )
 }
 
 export default CheckoutSideMenu
--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -2,7 +2,7 @@ import { XMarkIcon } from "@heroicons/react/24/solid"
 
 const OrderCard = props => {
     // eslint-disable-next-line react/prop-types
-    const { title, imageUrl, price } = props
+    const { id, title, imageUrl, price, handleDelete } = props
     return (
         <div className="flex justify-between items-center mb-2">
             <div className="flex items-center gap-2">
@@ -13,7 +13,7 @@ const OrderCard = props => {
             </div>
             <div className="flex items-center gap-2">
                 <p className="text-lg font-medium">{price}</p>
-                <XMarkIcon className="h-6 w-6  text-black cursor-pointer" />
+                <XMarkIcon onClick={()=>handleDelete(id)} className="h-6 w-6  text-black cursor-pointer" />
             </div>
         </div>
     )
