d8cde61474899eed0d3e54e96d6ff6bfc43d7d54
Juan <jcalderongomez@gmail.com>
2025-01-26 23:08:26 -0500

    clase 16

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -2,23 +2,36 @@ import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 import './styles.css'
+import OrderCard from '../OrdenCard'
 
 const CheckoutSideMenu = () => {
-  const context = useContext(ShoppingCartContext)
+    const context = useContext(ShoppingCartContext)
+    console.log('CART: ', context.cartProducts)
 
-  return (
-    <aside
-      className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
-      <div className='flex justify-between items-center p-6'>
-        <h2 className='font-medium text-xl'>My Order</h2>
-        <div>
-          <XMarkIcon
-            className='h-6 w-6 text-black cursor-pointer'
-            onClick={() => context.closeCheckoutSideMenu()}></XMarkIcon>
-        </div>
-      </div>
-    </aside>
-  )
+    return (
+        <aside
+            className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
+            <div className='flex justify-between items-center p-6'>
+                <h2 className='font-medium text-xl'>My Order</h2>
+                <div>
+                    <XMarkIcon
+                        className='h-6 w-6 text-black cursor-pointer'
+                        onClick={() => context.closeCheckoutSideMenu()}></XMarkIcon>
+                </div>
+            </div>
+            <div className='px-6'>
+                {context.cartProducts.map(product => (
+                    <OrderCard
+                        key={product.id}
+                        title={product.title}
+                        imageUrl={product.images}
+                        price={product.price}
+                    />
+                ))}
+
+            </div>
+        </aside>
+    )
 }
 
 export default CheckoutSideMenu
